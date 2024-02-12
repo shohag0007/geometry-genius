23463b338aac9c5b8126e29e7d9cfb587879d117
shohag0007 <merinedrive007@gmail.com>
2024-02-13 01:17:26 +0600

    parallelogram

--- rectangle-area.js
+++ rectangle-area.js
@@ -1,3 +1,24 @@
 function rectangleCalculateArea(){
-    console.log('go');
+    // get length of the rectangle.
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // get width of the rectangle.
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    // calculate rectangle area.
+    const area = width * length;
+    console.log('area of the rectangle:', area);
+
+    // Display rectangle area.
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
+
+
 }
